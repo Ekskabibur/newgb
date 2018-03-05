b25997fdf746d52cf37edfb01fb286993fa0fa1a
Ekskabibur <33994109+Ekskabibur@users.noreply.github.com>
2018-03-05 11:39:22 +0700

    Update channel.py

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/7.18.0"
-    LA = "CHROMEOS\t7.18.0\tChrome_OS\t1"
+    UA = "IOSIPAD 8.8.2 HELLO-WORLD 12.0.2"
+    LA = "LINE/2018.07332.2233.Tanduri/760.1.6 Tanysyz/18.0.0"
 
     authToken = None
     mid = None
