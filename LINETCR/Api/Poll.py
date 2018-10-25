b1f97b80ab1c783835476be6f0ed54a849638158
Ekskabibur <33994109+Ekskabibur@users.noreply.github.com>
2018-10-25 11:09:54 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,7 +18,7 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  AppName/LA :IOSIPAD 8.2.2 HELLO-WORLD 12.0.2
+  AppName/LA : IOSIPAD	8.11.0	Eater	1
   UserAgent/UA :  Line/2018.07332.2233.Tanduri/760.1.6 Tanysyz/18.0.0
 
   rev = 0
